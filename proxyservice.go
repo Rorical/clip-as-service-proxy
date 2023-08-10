d12dfa15f1055f00c1b9ac2be043a7c5615ccb30
Rorical <46294886+Rorical@users.noreply.github.com>
2023-08-10 11:02:23 +0800

    Update proxyservice.go

--- proxyservice.go
+++ proxyservice.go
@@ -3,14 +3,21 @@ package main
 import (
 	"flag"
 	"github.com/Rorical/clip-as-service-proxy/proxyservice"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	var configPath string
 	var addr string
+	var debug bool
 	flag.StringVar(&configPath, "config", "config.yaml", "Config File Path")
 	flag.StringVar(&addr, "address", ":50051", "Service Listen Address")
+	flag.BoolVar(&debug, "debug", false, "Debug Log Level")
 	flag.Parse()
+
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
 
 	panic(proxyservice.Serve(addr, configPath))
 }
